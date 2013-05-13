f3f356f1863b7d288390b67265c9613f277d0c9e
vagnervjs <vagnervjs@gmail.com>
2013-05-13 19:24:28 -0300

    Send infoeste2013

---
+++ infoeste2013/slide_config.js
@@ -0,0 +1,33 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Padrões para Criação e Interpretação de Conteúdos Web com HTML5',
+    subtitle: '',
+    eventInfo: {
+     title: 'Infoeste 2013',
+     date: '14/05/2013'
+    },
+    useBuilds: true,
+    usePrettify: true,
+    enableSlideAreas: true,
+    enableTouch: true,
+    analytics: 'UA-32909284-1',
+    favIcon: 'images/HTML5_Badge_32.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['personalizado']
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Vagner Santana',
+    company: 'Web Developer at Associação Educacional Prudentina',
+    gplus: 'http://plus.google.com/108752857690265245030',
+    twitter: '@vagnervjs',
+    www: 'http://www.vagnersantana.com',
+    github: 'http://github.com/vagnervjs'
+  }]
+};
+
